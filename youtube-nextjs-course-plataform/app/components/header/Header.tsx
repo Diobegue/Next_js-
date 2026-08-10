5952286529275fe8260451271b632dc4e948b173
Diobergue Viana <dioberguev@gmail.com>
2026-08-10 20:26:14 -0300

    feat: add responsive Header

--- youtube-nextjs-course-plataform/app/components/header/Header.tsx
+++ youtube-nextjs-course-plataform/app/components/header/Header.tsx
@@ -1,6 +1,6 @@
 'use client';
 import Link from "next/link";
-import { MdOpenInNew } from "react-icons/md";
+import { MdOpenInNew, MdMenu } from "react-icons/md";
 import { usePathname } from 'next/navigation';
 
 
@@ -8,24 +8,29 @@ export const Header = () => {
     const currentPath = usePathname()
 
     return (
-        <nav className="flex items-center justify-center bg-[var(--color-primary)] py-4 ">
+        <nav className="flex items-center gap-4 justify-start md:justify-center bg-[var(--color-primary)] py-2 sm:py-4 px-6">
+            
+            <button className="sm:hidden">
+                <MdMenu className="size-8"/>
+            </button>
             <ul className="flex gap-4 list-none items-center">
                 <li className='my-2'>
                     <Link href="/" className='border-2 rounded-md p-2 font-bold'>CODARSE</Link>
                 </li>
-                <li>
+                <li className="hidden sm:block">
                     <Link href="/" data-active={currentPath === '/'} className='data-[active=true]:underline'>Página inicial</Link>
                 </li>
-                <li>
+                <li  className="hidden sm:block">
                     <Link href="/cursos" data-active={currentPath === '/cursos'} className='data-[active=true]:underline'>Cursos</Link>
                 </li>
-                <li>
+                <li  className="hidden sm:block">
                     <Link href="https://blog.codarse.com" target="_blank" className='flex gap-1 items-center'>
                     Blog
                     <MdOpenInNew />
                     </Link>
                 </li>
             </ul>
+            <h1 className="sm:hidden">Página inicial</h1>
         </nav>
     )
 }
